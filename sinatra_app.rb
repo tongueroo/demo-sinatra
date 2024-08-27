e2c7f2fe8d51c4930be028ede0650e590c4bfd9f
Tung Nguyen <tongueroo@gmail.com>
2024-08-27 21:02:16 +0000

    x forwarded headres

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -7,6 +7,12 @@ class SinatraApp < Sinatra::Base
     client_ip = request.ip
     request_host = request.host
     puts "#{Time.now} - #{client_ip} - #{request_host} - GET /#{formatted_params}"
+
+    # Print X-Forwarded-* headers
+    puts "X-Forwarded-For: #{request.env["HTTP_X_FORWARDED_FOR"]}"
+    puts "X-Forwarded-Host: #{request.env["HTTP_X_FORWARDED_HOST"]}"
+    puts "X-Forwarded-Proto: #{request.env["HTTP_X_FORWARDED_PROTO"]}"
+
     erb :index
   end
 end
