c8f0ac5bc30cbedb3b8b17a5ddc848ef0691ad20
Tung Nguyen <tongueroo@gmail.com>
2024-08-27 20:45:09 +0000

    show request host

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -5,7 +5,8 @@ class SinatraApp < Sinatra::Base
     query_params = request.query_string
     formatted_params = query_params.empty? ? "" : "?" + query_params
     client_ip = request.ip
-    puts "#{Time.now} - #{client_ip} - GET /#{formatted_params}"
+    request_host = request.host
+    puts "#{Time.now} - #{client_ip} - #{request_host} - GET /#{formatted_params}"
     erb :index
   end
 end
