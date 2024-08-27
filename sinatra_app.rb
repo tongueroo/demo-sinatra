61431a8494da70b9496f48765103197314ceb358
Tung Nguyen <tongueroo@gmail.com>
2024-08-27 20:38:40 +0000

    puts client ip

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -4,7 +4,8 @@ class SinatraApp < Sinatra::Base
   get "/" do
     query_params = request.query_string
     formatted_params = query_params.empty? ? "" : "?" + query_params
-    puts "#{Time.now} - GET /#{formatted_params}"
+    client_ip = request.ip
+    puts "#{Time.now} - #{client_ip} - GET /#{formatted_params}"
     erb :index
   end
 end
