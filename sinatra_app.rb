8f59df1f31d3801e42fb554727529d717ac9be9c
Tung Nguyen <tongueroo@gmail.com>
2024-09-18 18:29:27 +0000

    debug show host headers

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -3,6 +3,14 @@ require "socket"
 
 class SinatraApp < Sinatra::Base
   get "/" do
+    # Print X-Forwarded-* headers
+    puts "X-Forwarded-Host: #{request.env["HTTP_X_FORWARDED_HOST"].inspect}"
+    puts "X-Forwarded-For: #{request.env["HTTP_X_FORWARDED_FOR"].inspect}"
+    puts "X-Forwarded-Proto: #{request.env["HTTP_X_FORWARDED_PROTO"].inspect}"
+    puts "X-Forwarded-Port: #{request.env["HTTP_X_FORWARDED_PORT"].inspect}"
+    puts "X-Real-IP: #{request.env["HTTP_X_REAL_IP"].inspect}"
+    # puts "Server Hostname: #{server_hostname}"
+
     query_params = request.query_string
     formatted_params = query_params.empty? ? "" : "?" + query_params
     client_ip = request.ip
@@ -10,14 +18,6 @@ class SinatraApp < Sinatra::Base
     server_hostname = Socket.gethostname
     puts "#{Time.now} - #{client_ip} - #{request_host} - #{server_hostname} -GET /#{formatted_params}"
     # puts "some message with a newline\nand more text"
-
-    # Print X-Forwarded-* headers
-    # puts "X-Forwarded-Host: #{request.env["HTTP_X_FORWARDED_HOST"]}"
-    # puts "X-Forwarded-For: #{request.env["HTTP_X_FORWARDED_FOR"]}"
-    # puts "X-Forwarded-Proto: #{request.env["HTTP_X_FORWARDED_PROTO"]}"
-    # puts "X-Forwarded-Port: #{request.env["HTTP_X_FORWARDED_PORT"]}"
-    # puts "X-Real-IP: #{request.env["HTTP_X_REAL_IP"]}"
-    # puts "Server Hostname: #{server_hostname}"
 
     erb :index
   end
